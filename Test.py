b5f2771eeb5408c6021405e3de87a32e4654c4a8
michelepelegrinelli <115238674+michelepelegrinelli@users.noreply.github.com>
2023-07-06 23:21:37 +0200

    Create Test.py

---
+++ Test.py
@@ -0,0 +1,37 @@
+import cv2
+import numpy as np
+import tensorflow as tf
+
+reloaded = tf.keras.models.load_model("./Modello", custom_objects={'KerasLayer':hub.KerasLayer})
+def predict_reload(image):
+    probabilities = reloaded.predict(np.asarray([img]))[0]
+    class_idx = np.argmax(probabilities)
+    return {Labels[class_idx]: probabilities[class_idx]}
+
+
+Labels=["benign", "malignant"]
+# Carica l'immagine
+image = cv2.imread("image.jpg") #image da sostituire con l'immagine che si vuole classificare
+
+# Ridimensiona l'immagine alle dimensioni desiderate
+resized_image = cv2.resize(image, (224, 224))
+
+# Normalizza i valori dei pixel nell'intervallo [0, 1]
+normalized_image = resized_image / 255.0
+
+# Aggiungi una dimensione batch all'immagine
+input_image = np.expand_dims(normalized_image, axis=0)
+
+# Esegue la previsione sul modello
+prediction = reloaded.predict(input_image)
+
+# Ottieni l'etichetta della classe predetta
+class_index = np.argmax(prediction)
+predicted_class = Labels[class_index]
+
+# Ottieni la confidenza della classe predetta
+confidence = prediction[0][class_index]
+
+# Stampa il risultato della previsione
+print("Classe predetta:", predicted_class)
+print("Confidenza:", confidence)
